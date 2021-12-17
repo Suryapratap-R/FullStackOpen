87de6399d7e9503c5dd679e4c4929b3b7a6040f0
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2021-12-17 09:12:59 +0530

    Exercise 1.2 added

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,11 +6,15 @@ const Header = (props) => (
  </h1>
 );
 
+const Part = (props) => (
+  <p>{props.part} {props.exercise}</p>
+)
+
 const Content = (props) => (
   <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <Part part={props.part1} exercise={props.exercises1}/>
+    <Part part={props.part2} exercise={props.exercises2}/>
+    <Part part={props.part3} exercise={props.exercises3}/>
   </>
 );
 
