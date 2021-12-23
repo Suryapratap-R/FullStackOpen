22f6c87ebd0237695a6fe738d6f02ccea6f0aa32
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2021-12-23 22:14:30 +0530

    Exercise 2.4 Added

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,61 +1,72 @@
 import React from 'react'
 
-// const Header = (props) => (
-//   <h1>
-//     {props.course}
-//  </h1>
-// );
-
-// const Part = (props) => (
-//   <p>{props.part} {props.exercise}</p>
-// )
-
-// const Content = (props) => (
-//   <>
-//     <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-//     <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-//     <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-//   </>
-// );
-
-// const Total = (props) => (
-//   <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-// )
-
-const Course = ({course}) => {
+const CourseModule = (course) => {
   return (
-    <>
-    <h1 key={course.id}>{course.name}</h1>
-      {course.parts.map((part) => <div key={part.id}>{part.name} { part.exercises }</div>)}
-    <strong>total of {course.parts.reduce((sum, part)=>part.exercises+sum, 0)} exercises</strong>
-    </>
+    <div key={course.id}>
+      <h2 key={course.id}>{course.name}</h2>
+          {course.parts.map((part) =>
+            <div key={part.id}>
+              {part.name} {part.exercises}
+            </div>
+            )}
+      <strong>total of {course.parts.reduce((sum, part)=>part.exercises+sum, 0)} exercises</strong>
+    </div>
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+const Course = ({ courses }) => {
+  return courses.map((c)=>CourseModule(c))
 
-  return <Course course={course} />
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+
+  return <Course courses={courses} />
 }
 
 export default App
