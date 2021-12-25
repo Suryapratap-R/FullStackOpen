6a28fe22eb4a4a437372ec393fa28f7b21094d17
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2021-12-25 21:19:44 +0530

    Exercise 2.10 Added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,7 +8,22 @@ const PhoneList = ({searchkey, p}) =>
       p.map((person) => <div key={person.name}>{person.name} {person.number}</div>) :
       p.filter((val) => val.name.toLowerCase().includes(searchkey.toLowerCase())).map((person) => <div key={person.name}>{person.name} {person.number} </div>)}
   </div>
-  }
+}
+
+const Filter = ({handleFilterChange}) => <div>
+          filter show with <input onChange={handleFilterChange}/>
+      </div>
+
+const PersonForm = (props) =>
+      <form onSubmit={props.addRecord}>
+        <div>
+          name: <input value={props.newName} onChange={props.handleNameChange}/>
+        </div>
+        <div>number: <input value={props.newNumber} onChange={props.handleNumberChange}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
 
 const App = () => {
   
@@ -24,8 +39,7 @@ const App = () => {
 
   const addRecord = (event) => {
     event.preventDefault()
-    console.log(persons.map((person) => person.name));
-    
+   
     if ((persons.map((person) => person.name).indexOf(newName)) === -1) {
       setPersons(persons.concat({ name: newName,  number: newNumber}))
       setNewName('')
@@ -45,25 +59,17 @@ const App = () => {
   const handleFilterChange = (event) => {
     setFilterWord(event.target.value)
   }
-// persons.filter((val)=>isPresent(val.name.toLowerCase().includes((event.target.value.toLowerCase()))))
   
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter show with <input onChange={handleFilterChange}/>
-        </div>
+      <Filter handleFilterChange = {handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addRecord}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addRecord={addRecord}
+        handleNameChange={handleNameChange}
+        newName={newName} newNumber={newNumber}
+        handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <PhoneList p={persons} searchkey={filterWord} />
     </div>
