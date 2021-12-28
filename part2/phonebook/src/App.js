f947c854d87d292efe630c1501d156c573846d4d
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2021-12-28 21:37:51 +0530

    Exercise 2.11 Added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 
 const PhoneList = ({searchkey, p}) =>
@@ -27,12 +28,7 @@ const PersonForm = (props) =>
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterWord, setFilterWord] = useState('')
@@ -47,8 +43,12 @@ const App = () => {
     } else {
       window.alert(`${newName} is already added to phone`)
     }
-
   }
+
+  const fetchApi = () => {
+    axios.get('http://localhost:3001/persons').then((response)=>setPersons(response.data))
+  }
+  useEffect(fetchApi, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
