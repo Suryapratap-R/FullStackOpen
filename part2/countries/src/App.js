ac4a8e7cd091f3937df751c019998fa60d3b9de1
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2021-12-30 16:28:37 +0530

    Exercise 2.14 Added

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,26 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const FilteredCountries = ({viewCountries, setViewCountries}) => {
-  if (viewCountries.length > 10) {
-    return 'Too many matches, specify another filter'
-  } else if (viewCountries.length === 1) {
-    const country = viewCountries[0]
-    return <>
+const DisplayCountry = (props) => {
+  const [temp, setTemp] = useState('')
+  const [icon, setIcon] = useState('')
+  const [speed, setSpeed] = useState('')
+
+   const effectData = () =>
+      axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+          .then((response) => 
+          {
+            setTemp(response.data.main.temp)
+            setSpeed(response.data.wind.speed)
+            setIcon('https://openweathermap.org/img/wn/'+response.data.weather[0].icon+'@2x.png')
+          }
+        )
+        useEffect(effectData, [])
+  
+  const country = props.country
+ 
+  return <>
       <h1>{country.name.common}</h1>
       <p>capital { country.capital }</p>
       <p>population {country.population}</p>
@@ -15,15 +29,24 @@ const FilteredCountries = ({viewCountries, setViewCountries}) => {
         {Object.values(country.languages).map((lang) => <li key={lang}>{ lang }</li>)}
       </ul>
       <div>{country.flag}</div>
-      <h3>Weather in { country.capital }</h3>
-    </>
-    
+      <h3>Weather in {country.capital}</h3>
+      <strong>temprature</strong> {temp} celcius <br />
+      <img src={ icon }/><br />
+      <strong>windspeed</strong> {speed} km/hr 
+    </>}
+
+const FilteredCountries = ({ viewCountries, setViewCountries }) => {
+  if (viewCountries.length > 10) {
+    return 'Too many matches, specify another filter'
+  } else if (viewCountries.length === 1) {
+    return <DisplayCountry country={ viewCountries[0] }/>
   }
   return viewCountries.map((country) =>
     <div key={country.name.official}>{country.name.common}
       <button onClick={()=>setViewCountries([country])}>show</button>
     </div>)
 }
+
 
 function App() {
   const [countries, setCountries] = useState([])
@@ -37,7 +60,6 @@ function App() {
 
   const handleFindCountry = (event) => {
     const word = event.target.value
-    console.log(countries.filter((country)=>country.name.common.toLowerCase().includes(word.toLowerCase())))
     setViewCountries(countries.filter((country)=>country.name.common.toLowerCase().includes(word.toLowerCase())))
   }
   
