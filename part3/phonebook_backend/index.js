3801ced28aac683cf8f4fdee1298c73681100a5a
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-03 18:26:38 +0530

    Exercise 3.3 Added

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,14 +27,24 @@ const persons = [
     }
 ]
 
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info of ${persons.length} people<br>${Date()}`)
+})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-    response.send(`Phonebook has info of ${persons.length} people<br>${Date()}`)
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
+
 
 const PORT = 3001
 app.listen(PORT)
