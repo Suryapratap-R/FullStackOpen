6dfefbc1f98a135fa6e4291d31c4aad601e4d470
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-09 09:06:31 +0530

    refectoring 2

--- part4/app.js
+++ part4/app.js
@@ -1,33 +1,15 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const blogSchema = require('./models/bloglist')
+const bloglistRouter = require('./controller/bloglist')
 const cors = require('cors')
 
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', bloglistRouter)
 
 module.exports = app
