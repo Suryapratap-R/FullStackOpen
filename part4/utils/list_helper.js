4bfc66677988b3fbe8f4dc2241bd1f2ccdcde3aa
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-10 09:52:49 +0530

    Exercise 4.4 Added

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPost) =>
+    blogPost.length === 0
+        ? 0
+        : blogPost.reduce((sum, blog) => sum + blog.likes, 0)
+
+
+
 module.exports = {
-  dummy
+    totalLikes,
+    dummy
 }
