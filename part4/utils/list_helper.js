9f2a3737c1188c9592a70300a444452583936322
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-10 18:36:11 +0530

    Exercise 4.6 & 4.7 Added

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,37 @@
+const _ = require('lodash');
+
+const mostBlogs = (blogList) => {
+  const authors = _.map(blogList, 'author')
+  const noBlog = _.mapValues( _.groupBy(authors), (o)=>o.length)
+
+  const maxBlogs = Object.entries(noBlog)
+    .reduce((prev, cur) => prev[1] > cur[1] ? prev : cur, [])
+
+  return maxBlogs.length === 0
+    ? {}
+    : {
+      author: maxBlogs[0],
+      blogs: maxBlogs[1]
+    }
+}
+
+const mostLikes = (blogList) => {
+  const groupAuthor = _.groupBy(blogList, 'author')
+  const authorLikes = _.mapValues(groupAuthor, (o) => o.reduce((prev, cur) => {
+      return prev + cur.likes;
+  }, 0)
+  )
+  const mostLiked = Object.entries(authorLikes).reduce((prev, cur) => {
+    return prev[1] > cur[1] ? prev : cur;
+  }, 0)
+  return mostLiked === 0
+    ? {}
+    : {
+    author: mostLiked[0],
+    likes: mostLiked[1]
+  }
+}
+
 const dummy = (blogs) => {
   return 1
 }
@@ -10,9 +44,10 @@ const totalLikes = (blogPost) =>
 const favorateBlog = (blogList) => blogList.find(blog => blog.likes === Math.max(...blogList.map((val) => val.likes)))
  
 
-
 module.exports = {
   totalLikes,
   dummy,
-  favorateBlog
+  favorateBlog,
+  mostBlogs,
+  mostLikes
 }
