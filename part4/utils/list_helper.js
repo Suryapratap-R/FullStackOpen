d1cc39ae6f79cc7482d329e1ad9d48586200dd21
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-10 11:53:25 +0530

    Exercise 4.5 Added

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,9 +7,12 @@ const totalLikes = (blogPost) =>
         ? 0
         : blogPost.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favorateBlog = (blogList) => blogList.find(blog => blog.likes === Math.max(...blogList.map((val) => val.likes)))
+ 
 
 
 module.exports = {
-    totalLikes,
-    dummy
+  totalLikes,
+  dummy,
+  favorateBlog
 }
