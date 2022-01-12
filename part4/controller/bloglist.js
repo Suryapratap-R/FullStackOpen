a2fa6f19776716e04a9925b24369be669efa2595
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-12 19:58:00 +0530

    look

--- part4/controller/bloglist.js
+++ part4/controller/bloglist.js
@@ -2,21 +2,8 @@ const bloglistRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 bloglistRouter.get('/', async (request, response) => {
-  // const blogs = await Blog.find({})
-  // console.log(blogs)
-
-  // response.json(blogs.map(blog => blog.toJSON()))
-
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-
-  // Blog.find({}).then(blogs => {
-  //   response.json(blogs)
-  // })
-
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 bloglistRouter.post('/', async (request, response) => {
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,31 +1,48 @@
 const supertest = require('supertest')
 const app = require('../app')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const api = supertest(app)
 
-const testfunc = async () => {
+// const testfunc = async () => {
     
-  const req = await api.get('/api/blogs')
-  console.log(req)
-}
+//   await api.get('http://localhost:3000/data').then(
+//     req => {
+//       console.log('running then')
+//       console.log(req)
+//     }
+//   )
+// }
 
-console.log('Execution Started')
-testfunc().then(() => {
-  console.log('DONE Execution')
-})
-// test('test bloglist returns json or not', async () => {
-//     console.log('here');
-//  const res = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
-//  console.log("res", res);
-// }, 10000)
-
-// test('verifying id property in json', async () => {
-//     const r = await api.get('/api/blogs')
-//     console.log(r);
-//     expect(r.body.id).toBeDefined()
-// }, 10000)
-
-// afterAll(() => {
-//     mongoose.connection.close()
+// console.log('Execution Started')
+// testfunc().then(() => {
+//   console.log('DONE Execution')
 // })
+// // test('test bloglist returns json or not', async () => {
+// //     console.log('here');
+// //  const res = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+// //  console.log("res", res);
+// // }, 10000)
+
+// // test('verifying id property in json', async () => {
+// //     const r = await api.get('/api/blogs')
+// //     console.log(r);
+// //     expect(r.body.id).toBeDefined()
+// // }, 10000)
+
+// // afterAll(() => {
+// //     mongoose.connection.close()
+// // })
+
+test('verify GET /api/blogs endpoint', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type',/application\/json/)
+
+  expect(response.body.length).toBe(3)
+}, 10000)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
