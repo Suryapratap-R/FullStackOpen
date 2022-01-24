92f9d773bcc3e5b8840a96691b011403d5b28eba
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-24 13:36:06 +0530

    Exercise 5.9 Added

--- part5/src/App.js
+++ part5/src/App.js
@@ -61,7 +61,7 @@ const App = () => {
 
     <div style={{ paddingTop: '20px' }}>
 
-      {blogs.map(blog =>
+      {blogs.sort((a, b)=>b.likes-a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateLike={updateLike}/>
       )}
     </div>
