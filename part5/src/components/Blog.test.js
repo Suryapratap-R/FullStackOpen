e3d8f711893654444da663ca378a14c5e8353064
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-01-25 17:53:24 +0530

    Exercise 5.14 Added

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,27 +1,41 @@
 import '@testing-library/jest-dom/extend-expect';
-import { prettyDOM, render } from '@testing-library/react';
+import { fireEvent, prettyDOM, render } from '@testing-library/react';
 import Blog from './Blog';
 
-test('<blog> title and author but does not render url or likes by default', () => {
-    const updateLike = jest.fn()
-    const deletePost = jest.fn()
 
-    const blogData = {
-        title: 'test title',
-        author: 'test author',
-        id: 'test id',
-        likes: 5,
-        url: 'test url'
-    }
+describe('<blog> ', () => {
 
-    const component = render(<Blog blog={blogData} updateLike={updateLike} deletePost={deletePost} />)
+    let component
 
-    const defaultVisible = component.container.querySelector('.blogDefault')
-    const defaultHidden = component.container.querySelector('.hiddenDefault')
-    
+    beforeEach(() => {
+        const updateLike = jest.fn()
+        const deletePost = jest.fn()
 
-    expect(defaultVisible).toHaveTextContent(
-        'test title test author'
-    )
-    expect(defaultHidden).toHaveStyle('display : none')
+        const blogData = {
+            title: 'test title',
+            author: 'test author',
+            id: 'test id',
+            likes: 5,
+            url: 'test url'
+        }
+
+        component = render(<Blog blog={blogData} updateLike={updateLike} deletePost={deletePost} />)
+    })
+
+    test('title and author but does not render url or likes by default', () => {
+        const defaultVisible = component.container.querySelector('.blogDefault')
+        const defaultHidden = component.container.querySelector('.hiddenDefault')
+        
+        expect(defaultVisible).toHaveTextContent(
+            'test title test author'
+        )
+        expect(defaultHidden).toHaveStyle('display : none')
+    })
+
+    test('blog url and likes are shown when button is clicked', () => {
+        const showButton = component.getByText('show')
+        fireEvent.click(showButton)
+        const defaultHidden = component.container.querySelector('.hiddenDefault')
+        expect(defaultHidden).not.toHaveStyle('display : none')
+    })
 })
